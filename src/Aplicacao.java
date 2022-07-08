07e68dcae8fc848655f2eb54863d6ec33e4d499d
F8223718 PEDRO BRAGA PLA <pedro.pla@bb.com.br>
2022-07-08 13:18:21 -0300

    classe abstrata Conta expandida com atributos dsaldo e donoConta

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -11,7 +11,10 @@ public class Aplicacao {
 		PessoaJuridica cliente2 = new PessoaJuridica();
 		cliente2.setNome("Super Pizzaria");
 		cliente2.setCnpj("45678912311");
-
+		
+		ContaCorrente conta1 = new ContaCorrente();
+		conta1.setSaldo(0);
+		conta1.setDonoConta(cliente1);
 	}
 
 }
--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,21 @@
 
 public abstract class Conta {
+	private double saldo;
+	private Cliente donoConta;
 	
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void setDonoConta(Cliente cliente) {
+		this.donoConta = cliente;
+	}
+	
+	public Cliente getDonoConta () {
+		return this.donoConta;
+	}
 }
